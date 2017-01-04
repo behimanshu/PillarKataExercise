4b87a73d3599572022473a6545b4a2be2aba2daa
behimanshu <hagarwal@uncc.edu>
2017-01-04 18:37:39 -0500

    Test case3 : Conversion of basic numerals to Roman Literals(Standard Refactored code)

--- src/RomanSource/RomanConversion.java
+++ src/RomanSource/RomanConversion.java
@@ -5,24 +5,35 @@ public class RomanConversion {
 	private enum DigitAssignment
 	{
 		//setting the values for each unique roman literal
-		I(1), V(5), X(10);
+		L(50),
+		X(10),
+		IX(9),
+		V(5),
+		IV(4),
+		I(1);
+		
 		private final int value;
 		
-		DigitAssignment(int value)
+		private DigitAssignment(int value1)
 		{
-			this.value = value;
+			value = value1;
 		}
+		
 	}
 	
 	public static String convertNumber(int number)
 	{
+		
 		for(DigitAssignment digit:DigitAssignment.values())
 		{
-			if(number==digit.value)
+			System.out.print("Entered for-->"+number);
+			if(number>=digit.value)
 			{
-				return digit.name();
+				System.out.println("Returned"+ digit.name() + convertNumber(number-digit.value));
+				return digit.name()+convertNumber(number- digit.value);
 			}
 		}
+		System.out.println("");
 		return "";	
 	}
 }
--- test/RomanTests/ConvertNumber.java
+++ test/RomanTests/ConvertNumber.java
@@ -1,6 +1,5 @@
 package RomanTests;
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -13,28 +12,44 @@ public class ConvertNumber {
 	public void converting0() {
 		assertConversionCheck("", 0);
 	}
-	
+
 	@Test
-	public void converting1()
-	{
+	public void converting1() {
 		assertConversionCheck("I", 1);
 	}
-	
+
 	@Test
-	public void converting5()
-	{
+	public void converting5() {
 		assertConversionCheck("V", 5);
 	}
-	
+
 	@Test
-	public void converting10()
-	{
+	public void converting10() {
 		assertConversionCheck("X", 10);
 	}
 
+	@Test
+	public void converting35() {
+		assertConversionCheck("XXXV", 35);
+	}
+
+	@Test
+	public void converting9() {
+		assertConversionCheck("IX", 9);
+	}
+
+	@Test
+	public void converting2() {
+		assertConversionCheck("II", 2);
+	}
 	
-	private void assertConversionCheck(String romanLiteral, int numeral)
+	@Test
+	public void converting57()
 	{
+		assertConversionCheck("LVII", 57);
+	}
+
+	private void assertConversionCheck(String romanLiteral, int numeral) {
 		assertEquals(romanLiteral, RomanConversion.convertNumber(numeral));
 	}
 
